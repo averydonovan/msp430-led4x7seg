9da34ec36bca375b914aa914a5ede8f4ef992f60
Donovan Smith <donovan@mccollinsmith.com>
2015-12-30 18:03:47 -0500

    Comments added, code format cleaned up

--- led4x7seg.c
+++ led4x7seg.c
@@ -1,19 +1,26 @@
 /*
- *  A
- * F B
- *  G
- * E C
- *  D  DP
+ * Demonstration of using a 4-character 7-segment LED display and multiple
+ * timers on the MSP430G2553
  */
 
 #include <msp430.h>
 #include <stdint.h>
 
 #define DLY_1S		4096 - 1
-#define DLY_500MS	4096/2 - 1
 #define DLY_512TH	7
 #define DLY_1024TH	3
 
+
+/*
+ * Bit  7  6  5  4  3  2  1  0
+ * Seg  DP A  B  C  D  E  F  G
+ *
+ *  AAA
+ * F   B
+ *  GGG
+ * E   C
+ *  DDD  DP
+ */
 const uint8_t segNums[] = {
 	0b01111110, // = 0
 	0b00110000, // = 1
@@ -27,14 +34,14 @@ const uint8_t segNums[] = {
 	0b01110011  // = 9
 };
 
-/*const byte segLets[7] = {
-  { 0,0,0,1,1,0,1 }, // = c
-  { 0,1,1,1,1,0,1 }, // = d
-  { 0,0,1,0,1,1,1 }, // = h
-  { 1,0,1,1,0,1,1 }, // = S
-  { 0,0,0,1,1,1,1 }, // = t
-  { 0,0,0,1,0,0,1 } // = =
-};*/
+const uint8_t segLets[7] = {
+	0b00001101, // = c
+	0b00111101, // = d
+	0b00010111, // = h
+	0b01011011, // = S
+	0b00001111, // = t
+	0b00001001  // = =
+};
 
 volatile uint8_t dispBuffer[4];
 volatile uint8_t dispCurDigit = 0;
@@ -46,6 +53,7 @@ void dispDigits(int_fast16_t num);
 void main(void) {
 	WDTCTL = WDTPW | WDTHOLD;		// Stop watchdog timer
 
+	// Initialize all ports by setting them to outputs, low
 	P1DIR = 0xFF;
 	P1OUT = 0;
 	P2DIR = 0xFF;
@@ -56,41 +64,43 @@ void main(void) {
 	BCSCTL1 = CALBC1_16MHZ; // Running at 16 MHz
 	DCOCTL = CALDCO_16MHZ;
 
-	BCSCTL1 |= DIVA_3; // Divide clock by 8
-	BCSCTL3 |= XCAP_3; // Set 12.5pF for crystal
+	BCSCTL1 |= DIVA_3; 	// Divide ACLK by 8
+	BCSCTL3 |= XCAP_3; 	// Set 12.5pF for crystal
 
-	//TACCR0 = 	DLY_500MS; 	// 500ms delay time
-	TA0CCR0 = 	DLY_512TH; 	// 1/512s delay time
-	TA0CCTL0 = 	CCIE; 		// Perform interrupt when time reached
-	// Use ACLK, count up mode, and divide clock by 8 again
-	TA0CTL |= 	TASSEL_1 | MC_1; //| ID_3;
+	// Timer for display refresh
+	TA0CCR0 = 	DLY_512TH; 			// 1/512s delay time
+	TA0CCTL0 = 	CCIE; 				// Perform interrupt when time reached
+	TA0CTL |= 	TASSEL_1 | MC_1; 	// Use ACLK, count up mode
 
-	TA1CCR0 = 	DLY_1S; 	// 1/512s delay time
-	TA1CCTL0 = 	CCIE; 		// Perform interrupt when time reached
-	TA1CTL |= 	TASSEL_1 | MC_1; //| ID_3;
+	// Timer for count up display
+	TA1CCR0 = 	DLY_1S; 			// 1s delay time
+	TA1CCTL0 = 	CCIE; 				// Perform interrupt when time reached
+	TA1CTL |= 	TASSEL_1 | MC_1; 	// Use ACLK, count up mode
 
 	uint_fast8_t count;
 
 	for (count = 4; count; --count)
-		dispBuffer[count] = 0x00;
+		dispBuffer[count] = 0x00;	// Initialize array
 
 	dispDigits(aNumber);
 
-	__enable_interrupt();
+	__enable_interrupt();	// Enable interrupts
 
 	while (1) {
-		__bis_SR_register(LPM3 | GIE);
+		__bis_SR_register(LPM3 | GIE);	// Set LPM3, allow intterupts on ACLK
 	}
 }
 
 #pragma vector=TIMER0_A0_VECTOR
 __interrupt void Timer_A0(void) {
-	dispCurDigit &= 0b00000011;
+	dispCurDigit &= 0b00000011; // Make sure value doesn't exceed 3
 
-	P1OUT = 0x00;
-	P2OUT = 0x0F;
+	P1OUT = 0x00;	// Clear P1
+	P2OUT = 0x0F;	// Set P2.0-2.3 high
 
+	// Only set cathode for digit being displayed to low
 	P2OUT &= ~((0b1000 >> dispCurDigit) | 0xF0);
+	// Display digit
 	P1OUT = dispBuffer[dispCurDigit];
 
 	dispCurDigit++;
@@ -120,8 +130,8 @@ void dispDigits(int_fast16_t num) {
 			num = num/10;
 		}
 	} else {
-		dispBuffer[0] = 0x01;
-		num = num * -1;
+		dispBuffer[0] = 0x01; 	// Minus sign as first character
+		num = num * -1;			// Need to turn into positive number now
 		for (count = 3; count; --count) {
 			dispBuffer[count] = segNums[(num%10)];
 			num = num/10;
